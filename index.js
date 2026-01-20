c7e710be4c1bdd279fcff2302af1202652b4a4be
SUMUKH L <sumukh.l@sixt.com>
2026-01-20 09:41:04 +0530

    Displays cover page when clicked on the book

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const book= document.getElementById("book1");
-const row = book.parentElement;
-
-const coverPage= document.createElement("div");
-coverPage.className= "coverpage";
-coverPage.innerHTML= "Hi how are you";
-
-const addCoverPage= (e) => {
-    row.appendChild(coverPage);
+const bookContent = {
+    "book1": "This is book1",
+    "book2": "This is book2",
+    "book3": "This is book3",
+    "book4": "This is book4",
+    "book5": "This is book5"
 };
 
-book.addEventListener("click", addCoverPage);
+const books = document.querySelectorAll(".book");
 
+books.forEach((book) => {
+    book.addEventListener("click", (event) => {
+        const clickedBook = event.currentTarget;
+        const bookId = clickedBook.id; 
+        
+        const coverPage = document.createElement("div");
+        coverPage.className = "coverpage";
+        
+        coverPage.innerHTML = bookContent[bookId] ;
+
+        clickedBook.parentElement.appendChild(coverPage);
+    });
+});
