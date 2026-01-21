b5f71e456954d47cff9ad7480c8c96a821ab93e3
Sumukh-L <sumukh.l@sixt.com>
2026-01-21 09:44:26 +0530

    Website made responsive

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is just in a begining stage right now. <br>
 It just shows a few books. Clicking on the Read Cover Page button displays the cover page of the book. <br>
 Clicking on a different book automatically closes the old cover page and displays the new one. <br>
 The cover pages are centred to view <br>
-Workig on adding more features and responsive layout!
+Made the website reponsive for different window sizes (minimizing/maximizing windw will not disturb the layout)
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bookContent = {
     She finds herself fighting not only for her country but for her life, and in the end, for the life of the man she loves...`,
 
 
+
     "book2": 
     `Darkness falls <br>
 
@@ -23,34 +24,39 @@ const bookContent = {
     Meanwhile, his cousin Roran must fight a new battle back home in Carvahall one that puts Eragon in even graver danger`,
 
 
+
     "book3": 
     `'Key is in the lock all right, sir. On the inside. Mr Ackroyd must have locked himself in.' <br>
 
     Poor Roger Ackroyd. He knew that the woman he loved had been harbouring a guilty secret - she poisoned her first husband. And yesterday she killed herself.
-
+    <br>
     But guilty secrets rarely stay secret. Who had been blackmailing her before her death? Had it really driven her to suicide? 
     And would it all be revealed in the letter that arrived in the evening post? Sadly Roger Ackroyd wasn't going to live long enough to find out...`,
+
 
 
     "book4": 
     `EVERY DAY THE SAME. UNTIL TODAY. <br>
 
-    Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens.
+    Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. <br>
 
     She's even started to feel like she knows the people who live in one of the houses. Their life as she sees it is perfect. If only Rachel could be that happy.
+    <br>
+    And then she sees something shocking, and in one moment everything changes. <br>
 
-    And then she sees something shocking, and in one moment everything changes.
+    Now Rachel has a chance to become a part of the lives she's only watched from afar. <br>
 
-    Now Rachel has a chance to become a part of the lives she's only watched from afar.
+    Now they'll see: she's much more than just the girl on the train...`,
 
-    Now they'll see: she's much more than just the girl on the train..`,
 
+
+    
     "book5": 
     `For Jack Reacher being invisible has become a habit. <br>
 
-    He spends his days digging swimming pools by hand and his nights as the bouncer in the local strip club in the Florida Keys.
+    He spends his days digging swimming pools by hand and his nights as the bouncer in the local strip club in the Florida Keys. <br>
 
-    He doesn't want to be found.
+    He doesn't want to be found. <br>
 
     But someone has sent a private detective to seek him out.
 
